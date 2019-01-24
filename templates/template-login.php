6081fd61dab1316bea197abb04f5790ba26199cc
Joe Ciavucco <joeciavucco@Joes-MBP.lan>
2019-01-24 22:03:07 +0000

    New logo

--- templates/template-login.php
+++ templates/template-login.php
@@ -4,7 +4,7 @@ get_header();
 ?>
     <section class="login">
         <div class="login-inner">
-            <span class="ion-ios-paw"></span>
+            <span class="logo"></span>
             <?php
             $args = array(
                 'redirect'       => ( is_ssl() ? 'https://' : 'http://' ) . $_SERVER['HTTP_HOST'],
